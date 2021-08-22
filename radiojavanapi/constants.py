1e05a758dfea635993e2d76ba69be749adb1e528
xHossein <77378345+xHossein@users.noreply.github.com>
2021-08-22 12:52:42 +0430

    Updated headers to latest app version

--- radiojavanapi/constants.py
+++ radiojavanapi/constants.py
@@ -1,3 +1,9 @@
 API_DOMAIN = "https://rjappapi.me/api2/{}"
-USER_AGENT = "Radio Javan/3.7.5 (Windows_NT 10.0.19041) com.RadioJavan.RJ.desktop"
-BASE_HEADERS = {'User-Agent':USER_AGENT}
+BASE_HEADERS = {
+    'Accept':'*/*',
+    'Accept-Language':'en-US',
+    'Accept-Encoding':'gzip, deflate',
+    'Host':'rjappapi.me',
+    'Connection':'keep-alive',
+    'User-Agent':'Radio Javan/3.7.8 (Windows_NT 10.0.19043) com.RadioJavan.RJ.desktop',
+}
