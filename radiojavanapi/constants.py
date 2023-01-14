e0428484e9c66e8a6a13394186a8f13f4a271705
xHossein <77378345+xHossein@users.noreply.github.com>
2023-01-14 13:47:22 +0330

    update headers & domain based on latest version of app

--- radiojavanapi/constants.py
+++ radiojavanapi/constants.py
@@ -1,11 +1,11 @@
-API_DOMAIN = "https://d3m6do6hyfiftd.cloudfront.net/api2/{}"
+API_DOMAIN = "https://rj-deskcloud.com/api2/{}"
 
 BASE_HEADERS = {
     'Accept': 'application/json, text/plain, */*',
     'Accept-Language': 'en-US',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Host': 'd3m6do6hyfiftd.cloudfront.net',
+    'Accept-Encoding': 'gzip, deflate',
+    'Host': 'rj-deskcloud.com',
     'Connection': 'keep-alive',
-    'x-rj-user-agent': 'Radio Javan/4.0.2 (Windows 10 64-bit 13.0.1) com.radioJavan.rj.desktop',
+    'x-rj-user-agent': 'Radio Javan/4.0.2/v4.0.3-1160-g4605067b (Windows 10 64-bit 13.0.1) com.radioJavan.rj.desktop',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) RadioJavan/4.0.2 Chrome/91.0.4472.69 Electron/13.0.1 Safari/537.36',
 }
